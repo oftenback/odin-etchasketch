3f40cc368e8c2a5b06c5facd4e1d665dfd0ab072
Peter <26168569+oftenback@users.noreply.github.com>
2022-10-24 20:41:33 -0400

    Fix an error which was keeping the pixels from displaying on the canvas
    I forgot that you have to create elements and then add class. In the
    previous commit I had created "col"s and "pixel"s instead of divs

--- script.js
+++ script.js
@@ -4,17 +4,18 @@
 let pixelCount = 16;
 
 function drawCanvas(pixels) {
-  const canvas = document.querySelector(".canvas");
+  const canvas = document.querySelector('.canvas');
   console.log(canvas);
   for (i = 0; i < pixels; i++) {
-    let column = document.createElement("col");
+    let column = document.createElement('div');
+    column.className = 'col';
     for (j = 0; j < pixels; j++) {
-      let pixel = document.createElement("pixel");
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
       column.appendChild(pixel);
     }
     canvas.appendChild(column);
   }
-  console.log("complete");
 }
 
 drawCanvas(pixelCount);
