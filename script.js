a2760843f1df2bd9d26414121cf03626a6fc67e7
Peter <26168569+oftenback@users.noreply.github.com>
2022-10-24 21:12:21 -0400

    Implement canvas pixels changing color after hover

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-//const btn = document.querySelector(".pixels");
+//const btn = document.querySelector(".size");
 //btn.addEventListener('click', getNewPixelCount);
 
 let pixelCount = 16;
 
-function drawCanvas(pixels) {
+function colorsIn(e) {
+  this.classList.add('colored');
+}
+
+function drawCanvas(size) {
   const canvas = document.querySelector('.canvas');
-  console.log(canvas);
-  for (i = 0; i < pixels; i++) {
+  for (i = 0; i < size; i++) {
     let column = document.createElement('div');
     column.className = 'col';
-    for (j = 0; j < pixels; j++) {
+    for (j = 0; j < size; j++) {
       let pixel = document.createElement('div');
       pixel.className = 'pixel';
       column.appendChild(pixel);
     }
     canvas.appendChild(column);
   }
+  const pixels = document.querySelectorAll('.pixel');
+  pixels.forEach((pixel) => pixel.addEventListener('mouseover', colorsIn));
 }
 
 function wipeCanvas() {
   const rows = document.querySelectorAll('.col');
   const canvas = document.querySelector('.canvas');
-  rows.forEach(row => {canvas.removeChild(row)});
+  rows.forEach((row) => {canvas.removeChild(row)});
 }
 
 drawCanvas(pixelCount);
